4988fbfa1c5086b46828f37faf9704a92ad534ad
oltahasimja <oh67751@ubt-uni.net>
2024-01-05 21:31:02 +0100

    contact us responsive

--- header.php
+++ header.php
@@ -3,10 +3,11 @@
         <div class="navigationbar">
             <img src="logo.png" class="logo">
             <ul class="header-ul">
-                <li><a href="cover.html">Home</a></li>
-                <li><a href="aboutus.html">About Us</a></li>
-                <li><a href="careers.html">Careers</a></li>
-                <li><a href="jobopenings.html">Job Openings</a></li>
+                <li><a href="cover.php">Home</a></li>
+                <li><a href="aboutus.php">About Us</a></li>
+                <li><a href="careers.php">Careers</a></li>
+                <li><a href="jobopenings.php">Job Openings</a></li>
+                <li><a href="contactus.php">Contact Us</a></li>
             </ul>
         </div>
     </header>
