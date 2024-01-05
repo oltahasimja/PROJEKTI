cd878108a1f32537777a116332d3098a22ff1f82
oltahasimja <oh67751@ubt-uni.net>
2024-01-05 16:15:22 +0100

    php

---
+++ aboutus.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="stylesheet" href="stylecover.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+</head>
+<body>
+    <header>   
+        <div class="navigationbar">
+       <img src="logo.png" class="logo">
+       <ul class="header-ul">
+        <li><a href="cover.html">Home</a></li>
+        <li><a href="aboutus.html">About Us</a></li>
+        <li><a href="careers.html">Careers</a></li>
+        <li><a href="jobopenings.html">Job Openings</a></li>
+       </ul>
+   </div></header>
+   <div class="banner2">
+        <div class="blloku1">
+
+        <img src="ndertesa.jpg" alt="">
+         
+        <div class="texti">
+            <p id="ourstorytitle">Our Story</p>
+         <br>
+            <div style="width: 20em;">
+                <p id="ourstory">
+                    A visionary team embarked on a journey seven years ago, driven by the mission to connect top-tier tech talent with organizations in need of cutting-edge solutions. <br>Thus, HIRE ME was born, a trailblazing company dedicated to shaping the future of technology across America and Europe.
+                    <br>Our team of dedicated professionals has scoured the tech hubs, identifying and recruiting the brightest minds in software development, data science, artificial intelligence, and beyond.         
+                </p>
+            </div>
+        </div>
+        </div>
+        <p id="why">Why We Started & Where We Are Now</p>
+        <div class="historia">
+      
+        <div class="vitet">
+            <div class="viti-2016">
+                <img src="2016.png" alt="">
+            </div>
+            <div class="viti-2020">
+                <img src="2020.png" alt="">
+            </div>
+            <div class="viti-2023">
+                <img src="2023.png" alt="">
+            </div>
+        </div>
+       
+        <div>
+            <div class="line"></div>
+
+            <div class="dot-div">
+                <div class="dot1"></div>
+                <div class="dot2"></div>
+                <div class="dot3"></div>
+            </div>
+            
+
+        </div>
+        <div class="div-3">
+            <div class="div-4"><p>
+                Microsystems acquires XRef, and evolves its market-leading proofreading solution, while strengthening the company’s UK presence.
+                
+                Microsystems opens its third office in New York City.
+                
+                Avaneesh Marwaha is named CEO of Microsystems.</p></div>
+                
+            <div class="div-5"><p>Microsystems, The Sackett Group, and HireMe Corp. merge to create HireMe Microsystems, with a portfolio of market-leading products for document drafting.
+
+                Microsystems relocates its global headquarters to downtown Chicago.
+                
+                Microsystems exceeds 300 firms globally using Contract Companion.</p></div>
+            <div class="div-6"><p>HireMe acquires Workshare and Doxly, creating a leading, global supplier of document drafting and transaction management technology.
+
+                HireMe launches HireMe Connect, its third-party technology partner program..</p></div>
+           
+        </div>
+        
+     </div>
+     <div class="work">
+        <p id="work-title">Work with Us</p>
+        <div class="tabelat">
+        <div class="tabela-1">
+           
+            <h2 >Data Driven</h2>
+         
+            <p id="parag1">In today's fast-paced and interconnected world, businesses and institutions rely on data-driven insights to gain a competitive edge and enhance efficiency.</p>
+            <img src="datadriven.png" alt="" class="data">
+        </div>
+        <div class="tabela-2">
+            <h2 >Innovation</h2>
+            <p id="parag1">In the realm of business, innovation sparks the development of cutting-edge products and services, driving competitiveness and market growth.</p>
+            <img src="inovacion.png" alt="" class="inovacion">
+        </div>
+        <div class="tabela-3">
+            <h2>Creativity</h2>
+            <p id="parag1">It empowers teams to tackle challenges with fresh perspectives, fostering an environment where bold ideas are embraced.</p>
+            <img src="creativity.png" alt="" class="creativity">
+        </div>
+        </div>
+        </div>
+
+        <div class="blloku2">
+        <div class="texti">
+        <p id="ourvalues">Our Values</p>
+     <br>
+        <p>
+            Our team of document experts will ensure that all phases of your HireMe-enabled document workflow runs efficiently and quickly. With a customer retention rate of over 98%, we take pride in supporting our customers in one of two packages: Standard and Premium 
+        </p>
+        </div>
+        <img src="values.jpg" alt="">
+     
+
+        </div>
+        <p id="services">Services</p>
+        <div class="services">
+    
+            <div class="service-1">
+                <img src="checkbox.png" alt="">
+                <p p id="kl">Inserting, re-arranging, removing, or hiding functionality on the Word ribbon</p>
+            </div>
+            <div class="service-2">
+                <img src="checkbox.png" alt="">
+                <p id="kl">Adding native or customized command buttons, groups, and tabs</p>
+            </div>
+       
+            <div class="service-3">
+                <img src="checkbox.png" alt="">
+                <p id="kl">Hiding native functionality and replacing or supplementing it with equivalent DocXtools functionality</p>
+            </div>
+            <div class="service-4">
+                <img src="checkbox.png" alt="">
+                <p id="kl">Intermixing custom macros on the Word ribbon</p>
+            </div>
+       
+        
+            <div class="service-5">
+                <img src="checkbox.png" alt="">
+                <p id="kl">Inserting non-Microsoft images and icons on the Word ribbon</p>
+            </div>
+            <div class="service-6">
+                <img src="checkbox.png" alt="">
+                <p id="kl">Editing screen tips and super tips across the Word ribbon</p>
+            </div>
+       
+       
+            <div class="service-7">
+                <img src="checkbox.png" alt="">
+                <p id="kl">Replacing Word’s convert to .docx feature with the safe DocXtools .doc to .docx functionality</p>
+            </div>
+            <div class="service-8">
+                <img src="checkbox.png" alt="">
+                <p id="kl">Hiding native Word functionality that is inappropriate for business users (including the Publish as Blog Post or Save to Web features)</p>
+            </div>
+        
+    </div>
+    <div class="team">
+        <img src="team.jpg" alt="">
+    </div>
+        </div>
+  
+
+        </div>
+   
+        </div>
+
+   <footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-col">
+                <h4>Company </h4>
+                <ul>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Privacy/Policy</a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>Contact us</h4>
+                <ul>
+                    <li><a href="#">hireme@work.com</a></li>
+                    <li><a href="#">+38344123123</a></li>
+                    <li><a href="#">+44 20 3453 4521</a></li>
+                    <li><a href="#">550 W Jackson Blvd Suite #200</a></li>
+                    <li><a href="#">153 Shemsi Mramori, Prishtinë</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>Socials </h4>
+                <div class="social-links">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-linkedin"></i></a>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,12 @@
+
+    <header>    
+        <div class="navigationbar">
+            <img src="logo.png" class="logo">
+            <ul class="header-ul">
+                <li><a href="cover.html">Home</a></li>
+                <li><a href="aboutus.html">About Us</a></li>
+                <li><a href="careers.html">Careers</a></li>
+                <li><a href="jobopenings.html">Job Openings</a></li>
+            </ul>
+        </div>
+    </header>
