8ceff7c95f450dcab78611bded5bbd410d40290c
invadoli <id67745@ubt-uni.net>
2024-01-10 19:37:48 +0100

    aboutus responsive

--- aboutus.php
+++ aboutus.php
@@ -17,7 +17,7 @@
         <div class="texti">
             <p id="ourstorytitle">Our Story</p>
          <br>
-            <div style="width: 20em;">
+            <div style="width: 20em;" class="div1">
                 <p id="ourstory">
                     A visionary team embarked on a journey seven years ago, driven by the mission to connect top-tier tech talent with organizations in need of cutting-edge solutions. <br>Thus, HIRE ME was born, a trailblazing company dedicated to shaping the future of technology across America and Europe.
                     <br>Our team of dedicated professionals has scoured the tech hubs, identifying and recruiting the brightest minds in software development, data science, artificial intelligence, and beyond.         
@@ -26,71 +26,71 @@
         </div>
         </div>
         <p id="why">Why We Started & Where We Are Now</p>
-        <div class="historia">
-      
-        <div class="vitet">
-            <div class="viti-2016">
-                <img src="2016.png" alt="">
+        <div class="divv1">
+            <div class="historia">
+                <div class="vitet">
+                    <div class="viti-2016">
+                        <img src="2016.png" alt="">
+                    </div>
+                    <div class="viti-2020">
+                        <img src="2020.png" alt="">
+                    </div>
+                    <div class="viti-2023">
+                        <img src="2023.png" alt="">
+                </div>
             </div>
-            <div class="viti-2020">
-                <img src="2020.png" alt="">
-            </div>
-            <div class="viti-2023">
-                <img src="2023.png" alt="">
-            </div>
-        </div>
-       
-        <div>
-            <div class="line"></div>
-
-            <div class="dot-div">
-                <div class="dot1"></div>
-                <div class="dot2"></div>
-                <div class="dot3"></div>
-            </div>
-            
-
-        </div>
-        <div class="div-3">
-            <div class="div-4"><p>
-                Microsystems acquires XRef, and evolves its market-leading proofreading solution, while strengthening the company’s UK presence.
-                
-                Microsystems opens its third office in New York City.
-                
-                Avaneesh Marwaha is named CEO of Microsystems.</p></div>
-                
-            <div class="div-5"><p>Microsystems, The Sackett Group, and HireMe Corp. merge to create HireMe Microsystems, with a portfolio of market-leading products for document drafting.
-
-                Microsystems relocates its global headquarters to downtown Chicago.
-                
-                Microsystems exceeds 300 firms globally using Contract Companion.</p></div>
-            <div class="div-6"><p>HireMe acquires Workshare and Doxly, creating a leading, global supplier of document drafting and transaction management technology.
-
-                HireMe launches HireMe Connect, its third-party technology partner program..</p></div>
-           
-        </div>
         
+            <div>
+                <div class="line"></div>
+
+                <div class="dot-div">
+                    <div class="dot1"></div>
+                    <div class="dot2"></div>
+                    <div class="dot3"></div>
+                </div>
+                
+
+            </div>
+            <div class="div-3">
+                <div class="div-4"><p>
+                    Microsystems acquires XRef, and evolves its market-leading proofreading solution, while strengthening the company’s UK presence.
+                    
+                    Microsystems opens its third office in New York City.
+                    
+                    Avaneesh Marwaha is named CEO of Microsystems.</p></div>
+                    
+                <div class="div-5"><p>Microsystems, The Sackett Group, and HireMe Corp. merge to create HireMe Microsystems, with a portfolio of market-leading products for document drafting.
+
+                    Microsystems relocates its global headquarters to downtown Chicago.
+                    
+                    Microsystems exceeds 300 firms globally using Contract Companion.</p></div>
+                <div class="div-6"><p>HireMe acquires Workshare and Doxly, creating a leading, global supplier of document drafting and transaction management technology.
+
+                    HireMe launches HireMe Connect, its third-party technology partner program..</p></div>
+            
+            </div>
+        </div>
      </div>
      <div class="work">
         <p id="work-title">Work with Us</p>
         <div class="tabelat">
-        <div class="tabela-1">
-           
-            <h2 >Data Driven</h2>
-         
-            <p id="parag1">In today's fast-paced and interconnected world, businesses and institutions rely on data-driven insights to gain a competitive edge and enhance efficiency.</p>
-            <img src="datadriven.png" alt="" class="data">
-        </div>
-        <div class="tabela-2">
-            <h2 >Innovation</h2>
-            <p id="parag1">In the realm of business, innovation sparks the development of cutting-edge products and services, driving competitiveness and market growth.</p>
-            <img src="inovacion.png" alt="" class="inovacion">
-        </div>
-        <div class="tabela-3">
-            <h2>Creativity</h2>
-            <p id="parag1">It empowers teams to tackle challenges with fresh perspectives, fostering an environment where bold ideas are embraced.</p>
-            <img src="creativity.png" alt="" class="creativity">
-        </div>
+            <div class="tabela-1">
+            
+                <h2 >Data Driven</h2>
+            
+                <p id="parag1">In today's fast-paced and interconnected world, businesses and institutions rely on data-driven insights to gain a competitive edge and enhance efficiency.</p>
+                <img src="datadriven.png" alt="" class="data">
+            </div>
+            <div class="tabela-2">
+                <h2 >Innovation</h2>
+                <p id="parag1">In the realm of business, innovation sparks the development of cutting-edge products and services, driving competitiveness and market growth.</p>
+                <img src="inovacion.png" alt="" class="inovacion">
+            </div>
+            <div class="tabela-3">
+                <h2>Creativity</h2>
+                <p id="parag1">It empowers teams to tackle challenges with fresh perspectives, fostering an environment where bold ideas are embraced.</p>
+                <img src="creativity.png" alt="" class="creativity">
+            </div>
         </div>
         </div>
 
@@ -110,50 +110,53 @@
         <div class="services">
     
             <div class="service-1">
-                <img src="checkbox.png" alt="">
+                <img src="checkbox.png" alt="" class="img-div">
                 <p p id="kl">Inserting, re-arranging, removing, or hiding functionality on the Word ribbon</p>
             </div>
             <div class="service-2">
-                <img src="checkbox.png" alt="">
+                <img src="checkbox.png" alt="" class="img-div">
                 <p id="kl">Adding native or customized command buttons, groups, and tabs</p>
             </div>
        
             <div class="service-3">
-                <img src="checkbox.png" alt="">
+                <img src="checkbox.png" alt="" class="img-div">
                 <p id="kl">Hiding native functionality and replacing or supplementing it with equivalent DocXtools functionality</p>
             </div>
             <div class="service-4">
-                <img src="checkbox.png" alt="">
+                <img src="checkbox.png" alt="" class="img-div">
                 <p id="kl">Intermixing custom macros on the Word ribbon</p>
             </div>
        
         
             <div class="service-5">
-                <img src="checkbox.png" alt="">
+                <img src="checkbox.png" alt="" class="img-div">
                 <p id="kl">Inserting non-Microsoft images and icons on the Word ribbon</p>
             </div>
             <div class="service-6">
-                <img src="checkbox.png" alt="">
+                <img src="checkbox.png" alt="" class="img-div">
                 <p id="kl">Editing screen tips and super tips across the Word ribbon</p>
             </div>
        
        
             <div class="service-7">
-                <img src="checkbox.png" alt="">
+                <img src="checkbox.png" alt="" class="img-div">
                 <p id="kl">Replacing Word’s convert to .docx feature with the safe DocXtools .doc to .docx functionality</p>
             </div>
             <div class="service-8">
-                <img src="checkbox.png" alt="">
+                <img src="checkbox.png" alt="" class="img-div">
                 <p id="kl">Hiding native Word functionality that is inappropriate for business users (including the Publish as Blog Post or Save to Web features)</p>
             </div>
         
     </div>
     <div class="team">
         <div class="meet">
-        <p id="meetourteam">Meet our team</p>
-        <a href="loginform.html" class="join"><h2>Join Us</h2></a>
-    </div>
-        <img src="team.jpg" alt="">
+            <p id="meetourteam">Meet our team</p>
+            <a href="loginform.html" class="join"><h2>Join Us</h2></a>
+        </div>
+        <div>
+            <img src="team.jpg" alt="" id="foto-team">
+        </div>
+        
     </div>
         
 
