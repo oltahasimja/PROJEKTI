b0f5a7f49cab0f7e6b4bf3141e24b828daed5531
invadoli <id67745@ubt-uni.net>
2024-01-29 16:06:00 +0100

    update careers sql

--- careers.sql
+++ careers.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 26, 2024 at 08:42 PM
+-- Generation Time: Jan 29, 2024 at 04:05 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -40,7 +40,9 @@ CREATE TABLE `careers` (
 --
 
 INSERT INTO `careers` (`ID`, `name`, `jobTitle`, `description`, `Img`) VALUES
-(3, 'Sophia Nguyen', 'Software Developer', 'In the capacity of Software Developer, Sophia Nguyen takes charge of driving the development efforts at HireMe Corporation. Leading a team of skilled programmers, Sophia oversees the design, implementation, and maintenance of high-quality software solutions. As an influential member of the management team, Sophia actively contributes to shaping the strategic direction and growth initiatives of the company. With a strong focus on technological innovation, Sophia plays a crucial role in positioning HireMe Corporation as a leader in cutting-edge programming and software development.', '');
+(11, 'Olivia Martinez', 'Network Engineer', 'In the role of Network Engineer, Olivia Martinez spearheads the design, implementation, and maintenance of HireMe Corporation\'s network infrastructure. With a focus on optimizing network performance, reliability, and security, Olivia leads a team of skilled engineers in ensuring seamless connectivity. As a pivotal member of the management team, she actively contributes to strategic planning and growth initiatives, playing a crucial role in the development and maintenance of HireMe Corporation\'s robust and secure network architecture. Olivia\'s expertise ensures the company\'s networks remain efficient and resilient in an ever-evolving technological landscape.', 0x706572736f6e342e6a7067),
+(12, 'Ethan Reynolds', 'Software Architect', 'As the Software Architect, Ethan Reynolds is at the forefront of HireMe Corporation\'s software design and development initiatives. Responsible for shaping the overall architecture and technical direction, Ethan leads a team of skilled engineers to create scalable and innovative software solutions. His role extends to overseeing software development processes, ensuring code quality, and driving technology innovation within the organization. A key influencer in the company\'s management team, Ethan actively contributes to strategic decisions and growth initiatives, cementing HireMe Corporation\'s position as a leader in cutting-edge software development.', 0x706572736f6e382e6a7067),
+(13, 'Sophia Nguyen', 'Software Developer', 'In the capacity of Software Developer, Sophia Nguyen takes charge of driving the development efforts at HireMe Corporation. Leading a team of skilled programmers, Sophia oversees the design, implementation, and maintenance of high-quality software solutions. As an influential member of the management team, Sophia actively contributes to shaping the strategic direction and growth initiatives of the company. With a strong focus on technological innovation, Sophia plays a crucial role in positioning HireMe Corporation as a leader in cutting-edge programming and software development.', 0x706572736f6e392e706e67);
 
 --
 -- Indexes for dumped tables
@@ -60,7 +62,7 @@ ALTER TABLE `careers`
 -- AUTO_INCREMENT for table `careers`
 --
 ALTER TABLE `careers`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
