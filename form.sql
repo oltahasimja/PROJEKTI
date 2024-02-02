e4a757899a25a0c0bbc4666926613f94b2c522b8
invadoli <id67745@ubt-uni.net>
2024-02-02 14:09:31 +0100

    form sql update

---
+++ form.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 02, 2024 at 02:08 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `register`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `form`
+--
+
+CREATE TABLE `form` (
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `roli` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `form`
+--
+
+INSERT INTO `form` (`name`, `email`, `pass`, `roli`) VALUES
+('oltaolta', 'olta123@gmail.com', '12341234', ''),
+('InvaDoli', 'id67745@ubt.net', '147852369', ''),
+('InvaDoli', 'id67745@ubt.net', '147852369', ''),
+('GretaAhma', 'greta.ahma@ubt-uni.net', '12345678', ''),
+('inva', 'id67745@ubt-uni.net', '12345678', 'admin'),
+('ElitaHajrizi', 'elita.hajrizaj@ubt-uni.net', '12345678', ''),
+('olta', 'olta123@gmail.com', '12345678', 'admin');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
